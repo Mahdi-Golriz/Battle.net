1b7b54a30bf96dd9560747f49f5d2537fdf5b05c
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-01-23 20:15:57 +0100

    change-js

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ menu()
 header()
 
 
+
+let topDiv = document.querySelector("div.top")
+topDiv.style.display = "none";
+topDiv.style.transition = "500ms"
+window.addEventListener("scroll", function () {
+    let st = this.scrollY || document.documentElement.scrollTop;
+    if (st > 600) {
+        topDiv.style.display = "block";
+        topDiv.style.transition = "500ms"
+     } else {
+        topDiv.style.display = "none";
+        topDiv.style.transition = "500ms"
+    }
+    });
+
+document.querySelector("div.top").addEventListener("click", function () {
+    // Scroll to the top with a smooth animation
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
