82200a045c5d484b4b4ea72f6743ab0fbabd5273
Mahdi-Golriz <mahdi.golriz.1993@gmail.com>
2024-01-23 20:29:44 +0100

    change-index

--- main.js
+++ main.js
@@ -4,27 +4,3 @@ import top from "./public/top/top";
 top()
 menu()
 header()
-
-
-
-let topDiv = document.querySelector("div.top")
-topDiv.style.display = "none";
-topDiv.style.transition = "500ms"
-window.addEventListener("scroll", function () {
-    let st = this.scrollY || document.documentElement.scrollTop;
-    if (st > 600) {
-        topDiv.style.display = "block";
-        topDiv.style.transition = "500ms"
-     } else {
-        topDiv.style.display = "none";
-        topDiv.style.transition = "500ms"
-    }
-    });
-
-document.querySelector("div.top").addEventListener("click", function () {
-    // Scroll to the top with a smooth animation
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
